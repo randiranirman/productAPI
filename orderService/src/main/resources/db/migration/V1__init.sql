d18726992fe1596c4c5767345cba7044cd1eab0f
randiranirman <randiranirman20011210@gmail.com>
2025-08-08 16:41:16 +0530

    added the migrations

---
+++ orderService/src/main/resources/db/migration/V1__init.sql
@@ -0,0 +1,8 @@
+CREATE TABLE t_orders (
+                          id BIGINT NOT NULL AUTO_INCREMENT,
+                          order_number VARCHAR(50) NOT NULL,
+                          sku_code VARCHAR(100) NOT NULL,
+                          price DECIMAL(19,2) NOT NULL,
+                          quantity INT NOT NULL,
+                          PRIMARY KEY (id)
+);
