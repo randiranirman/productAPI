f8b1dd0f04c820e9b198f34d26e934fb2e4a39f2
randiranirman <randiranirman20011210@gmail.com>
2025-08-08 16:15:35 +0530

    fixed the depenedancy issue in lombok

---
+++ orderService/src/main/java/org/moviecrudspring/orderservice/Models/Order.java
@@ -0,0 +1,31 @@
+package org.moviecrudspring.orderservice.Models;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "t_orders")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id ;
+
+    private String orderNumber ;
+    private BigDecimal price ;
+    private int quantity;
+    private String skuCode;
+
+
+}
