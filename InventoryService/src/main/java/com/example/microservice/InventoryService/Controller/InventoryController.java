279bbbc9122533bd4d854fa5f519904b83bd4a78
randiranirman <randiranirman20011210@gmail.com>
2025-08-09 15:09:02 +0530

    update the controller

--- InventoryService/src/main/java/com/example/microservice/InventoryService/Controller/InventoryController.java
+++ InventoryService/src/main/java/com/example/microservice/InventoryService/Controller/InventoryController.java
@@ -8,6 +8,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RequestMapping("/api/inventory")
 @RestController
 @RequiredArgsConstructor
@@ -20,15 +22,35 @@ public class InventoryController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public String createInventory(@RequestBody InventoryRequest inventoryRequest) {
-
-        inventoryService.createInventory(inventoryRequest.SkuCode(), inventoryRequest.Quantity());
-
-        return "Inventory created successfully for SKU: " + inventoryRequest.SkuCode();
+    public InventoryRequest createInventory(@RequestBody InventoryRequest inventoryRequest) {
 
 
 
 
+            var response = inventoryService.createInventory(inventoryRequest.skuCode(), inventoryRequest.Quantity());
 
+
+        return response;
+
+
+
+
+
+    }
+
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<InventoryRequest> getAllRequest() {
+
+        return inventoryService.getAllRequest();
+
+    }
+
+
+    @GetMapping("/inventory-check")
+    @ResponseStatus(HttpStatus.OK)
+    public boolean isInStock(@RequestParam String skuCode, @RequestParam int quantity) {
+        return inventoryService.isInStock(skuCode, quantity);
     }
 }
