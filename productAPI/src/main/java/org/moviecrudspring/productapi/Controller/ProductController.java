7a2a50a3f50618bb7b5333d8b97fc835dbd60d6c
randiranirman <randiranirman20011210@gmail.com>
2025-08-11 13:08:30 +0530

    added API-gateway to communicate between services

--- productAPI/src/main/java/org/moviecrudspring/productapi/Controller/ProductController.java
+++ productAPI/src/main/java/org/moviecrudspring/productapi/Controller/ProductController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/v1/products")
+@RequestMapping("/api/products")
 @RequiredArgsConstructor
 public class ProductController {
 
